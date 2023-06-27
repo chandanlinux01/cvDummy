b43da8c19b92330c591174a0770a877ecaae0761
Imravirathore <ravirathore672@gmail.com>
2023-06-27 17:43:21 +0530

    add new cv parser file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
+import AdminLayout from "layouts/Admin.js";
+import Home from "Home";
+import SignIn from "views/SignIn";
+import SignUp from "views/SignUp";
+import ProtectedRout from "components/Navbars/ProtectedRout";
+import ForgotPassword from "views/ForgotPassword";
+import ResetPassword from "views/ResetPassword";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <Router>
+    <Switch>
+      <Route exact path="/" render={(props) => <Home/>} />
+      <Route path="/signin" render={(props) => <SignIn />} />
+      <Route path="/signup" render={(props) => <SignUp />} />
+      <Route path="/forgot-password" render={(props) => <ForgotPassword />} />
+      <Route path="/password-reset" render={(props) => <ResetPassword />} />
+      <Route path="/admin" render={(props) => <ProtectedRout Component={AdminLayout }/> } />
+    </Switch>
+  </Router>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  );
